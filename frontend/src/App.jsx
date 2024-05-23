61a08e17beeebeb47ff672dcd7a9055e88d3d1e2
AniketKatre <aniketkatre3@gmail.com>
2024-05-24 00:04:10 +0530

    Few Ui Component............ added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import HomePage from "./components/Home/HomePage";
 import PublicNavbar from "./components/Navbar/PublicNavbar";
 import Login from "./components/User/Login";
 import Register from "./components/User/Register";
+import UpdatePassword from "./components/User/UpdatePassword";
+import UserProfile from "./components/User/UserProfile";
 
 export default function App() {
   return (
@@ -13,6 +15,8 @@ export default function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/change-password" element={<UpdatePassword />} />
+          <Route path="/user-profile" element={<UserProfile />} />
         </Routes>
       </BrowserRouter>
     </>
